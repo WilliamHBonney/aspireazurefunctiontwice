92aeb11bf749a4642c93d9d5a2fec9ab8a7d1949
William H Bonney <19433524+WilliamHBonney@users.noreply.github.com>
2025-10-30 18:28:42 +0000

    Simplest app

---
+++ AppHost/AppHost.cs
@@ -0,0 +1,31 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = DistributedApplication.CreateBuilder(args);
+
+        builder.AddAzureFunctionsProject<Projects.FunctionApp1>("functionapp1-first")
+            .WithArgs($"--verbose")
+            .WithEndpoint("http", (endpointBuilder) =>
+            {
+                endpointBuilder.Port = 6074;
+                endpointBuilder.TargetPort = 6074;
+                endpointBuilder.IsProxied = false;
+            })
+            .WithEnvironment("WEBSITE_HOSTNAME", "localhost:6074")
+            .WithEnvironment("ASPNETCORE_URLS", "http://+:6074");
+
+        builder.AddAzureFunctionsProject<Projects.FunctionApp1>("functionapp1-second")
+            .WithArgs($"--verbose")
+            .WithEndpoint("http", (endpointBuilder) =>
+            {
+                endpointBuilder.Port = 6075;
+                endpointBuilder.TargetPort = 6075;
+                endpointBuilder.IsProxied = false;
+            })
+            .WithEnvironment("WEBSITE_HOSTNAME", "localhost:6075")
+            .WithEnvironment("ASPNETCORE_URLS", "http://+:6075");
+
+        builder.Build().Run();
+    }
+}
